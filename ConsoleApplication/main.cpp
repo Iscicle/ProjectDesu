453410edab6fa888f37e05b23d781116691bea74
Iscicle <nhoj.adams@gmail.com>
2016-06-08 13:17:21 -0500

    Version 1

---
+++ ConsoleApplication/main.cpp
@@ -0,0 +1,10 @@
+#include "SFML/Graphics.hpp"
+
+int main(int argc, char** argv)
+{
+	sf::RenderWindow renderWindow(sf::VideoMode(640, 480), "test");
+	while (true){
+		renderWindow.clear();
+		renderWindow.display();
+	}
+}
